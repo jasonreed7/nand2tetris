e0fbc32e4cb962e4c3b81bbff5b63de1703cb947
Jason Reed <jreed0719@gmail.com>
2025-06-28 02:30:20 -0400

    Add solutions for project 4

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,42 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@i
+M=0
+
+@sum
+M=0
+
+(LOOP)
+@R1
+D=M
+
+@i
+D=D-M
+
+@END
+D;JEQ
+
+@R0
+D=M
+
+@sum
+M=D+M
+
+@i
+M=M+1
+
+@LOOP
+0;JMP
+
+(END)
+
+@sum
+D=M
+
+@R2
+M=D
+
+@END
+0;JMP
